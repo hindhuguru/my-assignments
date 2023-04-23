43625e7faf3b04db00a67e5d83416fbc9c3d8cc7
hindhuguru <131575165+hindhuguru@users.noreply.github.com>
2023-04-23 13:00:51 +0530

    Add files via upload

---
+++ browser.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class browser { 
+	
+	public static void main(String[] args) {
+		System.out.println("THIS IS MY BROWSER");
+		
+		Chrome obj=new Chrome();
+		 obj.getname();
+		 obj.printname();
+		
+
+		
+	}
+
+}
